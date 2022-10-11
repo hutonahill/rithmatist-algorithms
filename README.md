8e7b0afee103e3e7966d4ea97f5ce33603738146
hutonahill <35947141+hutonahill@users.noreply.github.com>
2022-10-10 23:06:47 -0600

    upgraded Warding Strength program to acomidate lists

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ some code for posible rithmatist game
 If you are interested in making a VR Rithmatist game. PLEASE reach out to me.
 
 https://coppermind.net/wiki/Rithmatics
+
+https://drive.google.com/drive/folders/11vS7-naQlsVj-2IHczNegiYc6wZBVw4L?usp=sharing
--- wardingStrength.py
+++ wardingStrength.py
@@ -1,6 +1,36 @@
 
-def main():
-    pass
+from msvcrt import SEM_NOALIGNMENTFAULTEXCEPT
+
+
+def main(circleCords:list, powerMod=1):
+    
+    # Create ouput list
+    ouput = []
+
+    # Loop through the circle cords and get the each arc group strength.
+    groupStr = []
+    for i in range(len(circleCords)):
+        
+        # Calculate group strength
+        copyData = calculateCurveHeight(circleCords[i-2], circleCords[i-1], 
+        circleCords[i])
+        
+        # Append data to list
+        groupStr.append(copyData)
+    
+    # Loop through agean to get combine two arc strengths to 
+    # get the arc strength of every segment.
+    segmentStrength = []
+    for i in range(len(groupStr)):
+
+        # Add agacent group strength then apply powerMod
+        copyData = (groupStr[i-1] + groupStr[i]) * powerMod
+
+        # Append data to list
+        segmentStrength.append(copyData)
+    
+    return segmentStrength
+
 
 def calculateCurveHeight(base1:tuple, tip:tuple, base2:tuple):
     import numpy
